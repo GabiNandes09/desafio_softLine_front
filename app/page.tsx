aaede0b5f99de61012b3d029519fb634f853a8eb
Nandes <fernandes.gabrielgon@gmail.com>
2025-06-25 18:31:27 -0300

    feat: login integrated

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button"
 import { useState } from "react"
 import { InputWithLabel } from "./_components/InputWithLabel"
 import { useRouter } from "next/navigation"
+import { toast } from "sonner"
 
 export default function LoginPage() {
   const [username, setUsername] = useState("")
@@ -22,7 +23,7 @@ export default function LoginPage() {
     router.replace("/pageOptions")
   }
 
-  const handleLogin = () => {
+  const handleLogin = async () => {
     const isUsernameValid = username.trim() !== ""
     const isPasswordValid = password.trim() !== ""
 
@@ -30,8 +31,42 @@ export default function LoginPage() {
     setPasswordError(!isPasswordValid)
 
     if (isUsernameValid && isPasswordValid) {
-      console.log("Login:", { username, password })
-      toOptions()
+      try {
+        const response = await fetch("http://172.28.255.81:8080/auth/login", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            username: username.trim(),
+            password: password.trim()
+          })
+        })
+
+        if (!response.ok) {
+          toast.error("Usuário ou senha inválidos")
+          return
+        }
+
+        const data = await response.json()
+        const token = data.token
+
+        if (!token) {
+          toast.error("Token não retornado pelo servidor")
+          return
+        }
+
+        // Salva o token no localStorage (persistente)
+        localStorage.setItem("authToken", token)
+
+
+        toast.success("Login realizado com sucesso")
+        toOptions()
+
+      } catch (error) {
+        toast.error("Erro ao conectar com o servidor")
+      }
     }
   }
 
